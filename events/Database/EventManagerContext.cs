e652ea6f0c3654b1900b3bf05053c1b0c06afda1
mmwahba <mmwahba@ejada.com>
2023-09-23 02:29:40 +0200

    added database configurations

---
+++ events/Database/EventManagerContext.cs
@@ -0,0 +1,14 @@
+﻿using events.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace events.Database
+{
+    public class EventManagerContext :DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+                 => options.UseSqlServer("Server=LPALXY0898;Database=Events;integrated security=False;Encrypt=False;Trusted_Connection=True;MultipleActiveResultSets=True;");
+    }
+}
